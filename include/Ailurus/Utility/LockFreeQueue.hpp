17c5cfc996fbcf62080f4e9463b8fdbe343ef5ac
keqing996 <epigrf@outlook.com>
2025-01-13 18:31:22 +0800

    Feat: lock free queue enqueue operation.

--- include/Ailurus/Utility/LockFreeQueue.hpp
+++ include/Ailurus/Utility/LockFreeQueue.hpp
@@ -7,16 +7,7 @@
 
 namespace Ailurus
 {
-    enum class LockFreeQueueStrategy
-    {
-        // SingleProducerSingleConsumer
-        SPSC,
-        // MultiProducerMultiConsumer
-        MPMC
-    };
-
-    template<typename T, LockFreeQueueStrategy Strategy = LockFreeQueueStrategy::MPMC, bool KeepOrder = false>
-    class LockFreeQueue
+    namespace _internal::LockFreeQueue
     {
         static constexpr uint32_t CACHE_LINE = 64;
 
@@ -35,7 +26,7 @@ namespace Ailurus
 
         public:
             explicit BucketArray(uint32_t size)
-                : _rawDataArray(::malloc(size * sizeof(Element)))
+                : _rawDataArray(static_cast<Element*>(::malloc(size * sizeof(Element))))
             {
                 ::memset(_rawDataArray, 0, size * sizeof(Element));
             }
@@ -66,127 +57,164 @@ namespace Ailurus
             Element* _rawDataArray;
         };
 
+        template<typename T>
+        class Storge
+        {
+        public:
+            explicit Storge(uint32_t wantedSize)
+                : size(UpAlignmentPowerOfTwo(wantedSize))
+                , data(size)
+                , state(size)
+            {
+            }
+
+        public:
+            // Padding index, avoid false sharing.
+            alignas(CACHE_LINE) std::atomic<uint32_t> headIndex {};
+            alignas(CACHE_LINE) std::atomic<uint32_t> tailIndex {};
+
+            uint32_t size;
+            BucketArray<T> data;
+            BucketArray<std::atomic<State>> state;
+
+        private:
+            static uint32_t UpAlignmentPowerOfTwo(uint32_t value)
+            {
+                if (value <= 4)
+                    return 4;
+
+                value--;
+                value |= value >> 1;
+                value |= value >> 2;
+                value |= value >> 4;
+                value |= value >> 8;
+                value |= value >> 16;
+
+                return value + 1;
+            }
+        };
+
+    }
+
+    enum class LockFreeQueueStrategy
+    {
+        // SingleProducerSingleConsumer
+        SPSC,
+        // MultiProducerMultiConsumer
+        MPMC
+    };
+
+    template<typename T, LockFreeQueueStrategy Strategy = LockFreeQueueStrategy::MPMC>
+    class LockFreeQueue;
+
+    template<typename T>
+    class LockFreeQueue<T, LockFreeQueueStrategy::SPSC>
+    {
+        using State = _internal::LockFreeQueue::State;
+        using Storge = _internal::LockFreeQueue::Storge<T>;
     public:
         explicit LockFreeQueue(uint32_t size)
-            : _size(UpAlignmentPowerOfTwo(size))
-            , _data(_size)
-            , _state(_size)
+            : _storge(size)
         {
         }
 
     public:
-        uint32_t GetSize() const
+        uint32_t GetSize()
         {
-            return _size;
+            return _storge.size;
         }
 
         void Enqueue(T&& data)
         {
-            if constexpr (Strategy == LockFreeQueueStrategy::SPSC)
+            // Safe get current head index
+            uint32_t headIndex = _storge.headIndex.load();
+            _storge.headIndex.store(headIndex + 1);
+
+            // Check state, wait state to be empty.
+            std::atomic<State>& state = _storge.state[headIndex];
+            while (state.load(std::memory_order_acquire) != State::Empty)
+                SpinPause();
+
+            // Set data.
+            _storge.data[headIndex] = std::forward<T>(data);
+
+            // Set state.
+            state.store(State::Loaded, std::memory_order_relaxed);
+        }
+
+        T Dequeue()
+        {
+        }
+
+        bool TryEnqueue(T&& data)
+        {
+        }
+
+        std::optional<T> TryDequeue()
+        {
+        }
+
+    private:
+        Storge _storge;
+    };
+
+
+    template<typename T>
+    class LockFreeQueue<T, LockFreeQueueStrategy::MPMC> : _internal::LockFreeQueue::Storge<T>
+    {
+        using State = _internal::LockFreeQueue::State;
+        using Storge = _internal::LockFreeQueue::Storge<T>;
+    public:
+        explicit LockFreeQueue(uint32_t size, bool keepOrder)
+            : _storge(size)
+            , _keepOrder(keepOrder)
+        {
+        }
+
+    public:
+        uint32_t GetSize()
+        {
+            return _storge.size;
+        }
+
+        void Enqueue(T&& data)
+        {
+            // Safe get current head index
+            uint32_t headIndex = _storge.headIndex.fetch_add(1, _keepOrder ? std::memory_order_seq_cst : std::memory_order_relaxed);
+
+            // Check state, wait state to be empty.
+            std::atomic<State>& state = _storge.state[headIndex];
+            while (true)
             {
-                // Safe get current head index
-                uint32_t headIndex = _headIndex.load();
-                _headIndex.store(headIndex + 1);
-
-                // Check state, wait state to be empty.
-                std::atomic<State>& state = _state[headIndex];
-                while (state.load(std::memory_order_acquire) != State::Empty)
-                    SpinPause();
-
-                // Set data.
-                _data[headIndex] = std::forward<T>(data);
-
-                // Set state.
-                state.store(State::Loaded, std::memory_order_relaxed);
-            }
-            else
-            {
-                // Safe get current head index
-                constexpr auto order = KeepOrder ? std::memory_order_seq_cst : std::memory_order_relaxed;
-                uint32_t headIndex = _headIndex.fetch_add(1, order);
-
-                // Check state, wait state to be empty.
-                std::atomic<State>& state = _state[headIndex];
-                while (true)
+                State expected = State::Empty;
+                if (state.compare_exchange_weak(expected, State::Enqueuing, std::memory_order_acquire, std::memory_order_relaxed))
                 {
-                    State expected = State::Empty;
-                    if (state.compare_exchange_weak(expected, State::Enqueuing, std::memory_order_acquire, std::memory_order_relaxed))
-                    {
-                        // Set data.
-                        _data[headIndex] = std::forward<T>(data);
+                    // Set data.
+                    _storge.data[headIndex] = std::forward<T>(data);
 
-                        // Set state.
-                        state.store(State::Loaded, std::memory_order_relaxed);
-                        return;
-                    }
-
-                    SpinPause();
+                    // Set state.
+                    state.store(State::Loaded, std::memory_order_relaxed);
+                    return;
                 }
+
+                SpinPause();
             }
         }
 
         T Dequeue()
         {
-            if constexpr (Strategy == LockFreeQueueStrategy::SPSC)
-            {
-
-            }
-            else
-            {
-
-            }
         }
 
         bool TryEnqueue(T&& data)
         {
-            if constexpr (Strategy == LockFreeQueueStrategy::SPSC)
-            {
-
-            }
-            else
-            {
-
-            }
         }
 
         std::optional<T> TryDequeue()
         {
-            if constexpr (Strategy == LockFreeQueueStrategy::SPSC)
-            {
-
-            }
-            else
-            {
-
-            }
         }
 
     private:
-        static constexpr
-        uint32_t UpAlignmentPowerOfTwo(uint32_t value)
-        {
-            if (value <= 4)
-                return 4;
-
-            value--;
-            value |= value >> 1;
-            value |= value >> 2;
-            value |= value >> 4;
-            value |= value >> 8;
-            value |= value >> 16;
-
-            return value + 1;
-        }
-
-
-
-    private:
-        // Padding index, avoid false sharing.
-        alignas(CACHE_LINE) std::atomic<uint32_t> _headIndex {};
-        alignas(CACHE_LINE) std::atomic<uint32_t> _tailIndex {};
-
-        uint32_t _size;
-        BucketArray<T> _data;
-        BucketArray<std::atomic<State>> _state;
+        Storge _storge;
+        bool _keepOrder;
     };
 }
