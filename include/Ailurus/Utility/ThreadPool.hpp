97e97f38b36ce0e5137b23d6a33f11a4d353023b
kq996 <epigrf@outlook.com>
2025-01-27 18:41:42 +0800

    Feat: threads pool and test code.
    Feat: threads pool and test code.

--- include/Ailurus/Utility/ThreadPool.hpp
+++ include/Ailurus/Utility/ThreadPool.hpp
@@ -2,18 +2,115 @@
 
 #include <vector>
 #include <queue>
-#include <memory>
 #include <thread>
-#include <mutex>
 #include <condition_variable>
-#include <future>
 #include <functional>
-#include <stdexcept>
 
 namespace Ailurus
 {
     class ThreadPool
     {
+    public:
+        explicit ThreadPool(size_t threadNum)
+        {
+            for (size_t i = 0; i < threadNum; ++i)
+            {
+                _threads.emplace_back([this]()->void
+                {
+                    Worker();
+                });
+            }
+        }
 
+        ~ThreadPool()
+        {
+            ShutDown();
+        }
+
+        void ShutDown()
+        {
+            {
+                std::unique_lock lock(_mutex);
+
+                if (_shutdown)
+                    return;
+
+                _shutdown = true;
+            }
+
+            _conditionVar.notify_all();
+
+            // Wait all tasks done
+            for (std::thread &worker: _threads)
+                worker.join();
+        }
+
+        bool Enqueue(const std::function<void()>& task)
+        {
+            {
+                std::unique_lock lock(_mutex);
+
+                if (_shutdown)
+                    return false;
+
+                _taskQueue.emplace(task);
+            }
+
+            _conditionVar.notify_one();
+            return true;
+        }
+
+    private:
+        void Worker()
+        {
+            while (true)
+            {
+                std::function<void()> task;
+
+                {
+                    std::unique_lock lock(_mutex);
+
+                    if (_shutdown)
+                    {
+                        if (_taskQueue.empty())
+                            return;
+
+                        task = std::move(_taskQueue.front());
+                        _taskQueue.pop();
+                    }
+                    else
+                    {
+                        if (!_taskQueue.empty())
+                        {
+                            task = std::move(_taskQueue.front());
+                            _taskQueue.pop();
+                        }
+                        else
+                        {
+                            _conditionVar.wait(lock,[this]() -> bool
+                            {
+                                return _shutdown || !_taskQueue.empty();
+                            });
+
+                            if (_taskQueue.empty())
+                                continue;
+
+                            task = std::move(_taskQueue.front());
+                            _taskQueue.pop();
+                        }
+                    }
+                }
+
+                task();
+            }
+        }
+
+    private:
+        std::vector<std::thread> _threads;
+        std::queue<std::function<void()>> _taskQueue;
+
+        std::mutex _mutex;
+        std::condition_variable _conditionVar;
+        bool _shutdown = false;
     };
 }
