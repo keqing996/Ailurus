1ae8bc8403b975e480b06ec55e4354b5a546b7a5
keqing996 <epigrf@outlook.com>
2025-04-01 11:29:01 +0800

    Feat: Matrix.

--- include/Core/Ailurus/Math/Matrix.hpp
+++ include/Core/Ailurus/Math/Matrix.hpp
@@ -2,7 +2,9 @@
 
 #include <cstring>
 #include <cmath>
+#include <concepts>
 #include <cstdint>
+#include "Vector.hpp"
 
 namespace Ailurus
 {
@@ -10,8 +12,8 @@ namespace Ailurus
 	class Matrix
 	{
 	public:
-		constexpr size_t RowSize = Row;
-		constexpr size_t ColSize = Col;
+		constexpr static size_t RowSize = Row;
+		constexpr static size_t ColSize = Col;
 
 		Matrix() = default;
 
@@ -54,35 +56,151 @@ namespace Ailurus
 			return *this;
 		}
 
-		std::array<ElementType, Col>& operator[](size_t rowIndex)
+		Vector<ElementType, Col>& operator[](size_t rowIndex)
 		{
 			return _elements[rowIndex];
 		}
 
-		const std::array<ElementType, Col>& operator[](size_t rowIndex) const
+		const Vector<ElementType, Col>& operator[](size_t rowIndex) const
 		{
 			return _elements[rowIndex];
-		}
-
-		template <size_t OtherCol>
-		Matrix<ElementType, Row, OtherCol> operator*(const Matrix<ElementType, Col, OtherCol>& other) const
-		{
-			Matrix<ElementType, Row, OtherCol> result;
-			for (size_t i = 0; i < Row; ++i)
-			{
-				for (size_t j = 0; j < OtherCol; ++j)
-				{
-					ElementType sum = ElementType{};
-					for (size_t k = 0; k < Col; ++k)
-						sum += _elements[i][k] * other[k][j];
-
-					result[i][j] = sum;
-				}
-			}
-			return result;
 		}
 
 	private:
-		std::array<std::array<ElementType, Col>, Row> _elements;
+		Vector<Vector<ElementType, Col>, Row> _elements;
 	};
+
+	namespace _internal
+	{
+		template <typename MatrixElementType, typename ScalarType>
+		concept ScalarMultiplication = requires(MatrixElementType t, ScalarType u) {
+			{ t* u } -> std::convertible_to<MatrixElementType>;
+		};
+
+	} // namespace _internal
+
+	template <typename ElementType, size_t Row, size_t Col, typename ScalarType>
+	Matrix<ElementType, Row, Col> operator*(
+		const Matrix<ElementType, Row, Col>& mat,
+		const ScalarType& scalar)
+		requires _internal::ScalarMultiplication<ElementType, ScalarType>
+	{
+		Matrix<ElementType, Row, Col> result;
+		for (size_t i = 0; i < Row; ++i)
+		{
+			for (size_t j = 0; j < Col; ++j)
+			{
+				result[i][j] = mat[i][j] * scalar;
+			}
+		}
+		return mat;
+	}
+
+	template <typename ElementType, size_t Row, size_t Col, typename ScalarType>
+	Matrix<ElementType, Row, Col> operator*(
+		const ScalarType& scalar,
+		const Matrix<ElementType, Row, Col>& mat)
+		requires _internal::ScalarMultiplication<ElementType, ScalarType>
+	{
+		return mat * scalar;
+	}
+
+	template <typename ElementType, size_t Row, size_t Col, typename ScalarType>
+	Matrix<ElementType, Row, Col>& operator*=(
+		Matrix<ElementType, Row, Col>& mat,
+		const ScalarType& scalar)
+		requires _internal::ScalarMultiplication<ElementType, ScalarType>
+	{
+		for (size_t i = 0; i < Row; ++i)
+		{
+			for (size_t j = 0; j < Col; ++j)
+			{
+				mat[i][j] *= scalar;
+			}
+		}
+		return mat;
+	}
+
+	template <typename ElementType, size_t Row, size_t Col, size_t OtherCol>
+	Matrix<ElementType, Row, OtherCol> operator*(
+		const Matrix<ElementType, Row, Col>& left,
+		const Matrix<ElementType, Col, OtherCol>& right)
+	{
+		Matrix<ElementType, Row, OtherCol> result;
+		for (size_t i = 0; i < Row; ++i)
+		{
+			for (size_t j = 0; j < OtherCol; ++j)
+			{
+				ElementType sum = ElementType{};
+				for (size_t k = 0; k < Col; ++k)
+					sum += left[i][k] * right[k][j];
+
+				result[i][j] = sum;
+			}
+		}
+		return result;
+	}
+
+	template <typename ElementType, size_t Row, size_t Col>
+	Matrix<ElementType, Row, Col> operator+(
+		const Matrix<ElementType, Row, Col>& left,
+		const Matrix<ElementType, Row, Col>& right)
+	{
+		Matrix<ElementType, Row, Col> result;
+		for (size_t i = 0; i < Row; ++i)
+		{
+			for (size_t j = 0; j < Col; ++j)
+			{
+				result[i][j] = left[i][j] + right[i][j];
+			}
+		}
+		return result;
+	}
+
+	template <typename ElementType, size_t Row, size_t Col>
+	Matrix<ElementType, Row, Col>& operator+=(
+		Matrix<ElementType, Row, Col>& left,
+		const Matrix<ElementType, Row, Col>& right)
+	{
+		for (size_t i = 0; i < Row; ++i)
+		{
+			for (size_t j = 0; j < Col; ++j)
+			{
+				left[i][j] += right[i][j];
+			}
+		}
+		return left;
+	}
+
+	template <typename ElementType, size_t Row, size_t Col>
+	Matrix<ElementType, Row, Col> operator-(
+		const Matrix<ElementType, Row, Col>& left,
+		const Matrix<ElementType, Row, Col>& right)
+	{
+		Matrix<ElementType, Row, Col> result;
+		for (size_t i = 0; i < Row; ++i)
+		{
+			for (size_t j = 0; j < Col; ++j)
+			{
+				result[i][j] = left[i][j] - right[i][j];
+			}
+		}
+		return result;
+	}
+
+	template <typename ElementType, size_t Row, size_t Col>
+	Matrix<ElementType, Row, Col>& operator-=(
+		Matrix<ElementType, Row, Col>& left,
+		const Matrix<ElementType, Row, Col>& right)
+	{
+		for (size_t i = 0; i < Row; ++i)
+		{
+			for (size_t j = 0; j < Col; ++j)
+			{
+				left[i][j] -= right[i][j];
+			}
+		}
+		return left;
+	}
+
 } // namespace Ailurus
