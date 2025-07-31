da1fd2cfb45cc67fbca0920e2a71966711d37e58
kq996 <epigrf@outlook.com>
2025-07-31 13:25:16 +0800

    [Feat] frame buffer

--- src/App/VulkanContext/FrameBuffer/VulkanFrameBufferManager.h
+++ src/App/VulkanContext/FrameBuffer/VulkanFrameBufferManager.h
@@ -1,25 +1,45 @@
 #pragma once
 
+#include <memory>
+#include <unordered_map>
 #include <vulkan/vulkan.hpp>
 #include <Ailurus/Utility/NonCopyable.h>
 #include <Ailurus/Utility/NonMovable.h>
 
 namespace Ailurus
 {
+    class VulkanFrameBuffer;
+    class VulkanRenderPass;
+    
     class VulkanFrameBufferManager : public NonCopyable, public NonMovable
     {
+        struct BackBufferKey
+        {
+            VulkanRenderPass* pRenderPass;
+            uint32_t imageIndex;
+        };
+
+        struct BackBufferKeyEqual
+        {
+            bool operator()(const BackBufferKey& lhs, const BackBufferKey& rhs) const;
+        };
+
+        struct BackBufferKeyHash
+        {
+            std::size_t operator()(const BackBufferKey& key) const;
+        };
+
+        using BackBufferMap = std::unordered_map<BackBufferKey, std::unique_ptr<VulkanFrameBuffer>, BackBufferKeyHash, BackBufferKeyEqual>;
+
     public:
         VulkanFrameBufferManager();
         ~VulkanFrameBufferManager();
 
     public:
-        void RecreateBackBuffers();
+        void ClearBackBuffers();
+        auto GetBackBuffer(VulkanRenderPass* pRenderPass, uint32_t imageIndex) -> VulkanFrameBuffer*;
 
     private:
-        void CreateBackBuffers();
-        void DestroyBackBuffers();
-
-    private:
-        std::vector<vk::Framebuffer> _vkBackBuffers;
+        BackBufferMap _vkBackBuffers;
     };
 }
